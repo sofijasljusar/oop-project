a81198c6d57558f62df41211118f2296c685271f
sofijasljusar <sofija.sljusar@gmail.com>
2025-04-28 16:41:29 +0300

    Start UI development. Create basic UI skeleton

--- src/main/java/com/plandiy/HelloApplication.java
+++ src/main/java/com/plandiy/HelloApplication.java
@@ -2,18 +2,33 @@ package com.plandiy;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+    double x,y = 0;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        Scene sc = new Scene(root);
+//        stage.initStyle(StageStyle.UNDECORATED);
+        root.setOnMousePressed(evt ->{
+            x = evt.getSceneX();
+            y = evt.getSceneY();
+        });
+        root.setOnMouseDragged(evt ->{
+            stage.setX(evt.getScreenX()-x);
+            stage.setY(evt.getScreenY()-y);
+        });
+
+        stage.setTitle("PlanDiy");
+        stage.setScene(sc);
+//        stage.setMaximized(true);
         stage.show();
     }
 

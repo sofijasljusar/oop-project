cdd7e0b8908ff027944ebc603ff251343afcf1f7
sofijasljusar <sofija.sljusar@gmail.com>
2025-05-03 00:48:37 +0300

    Add modal for editing task or viewing details

---
+++ src/main/java/com/plandiy/controller/EditTaskController.java
@@ -0,0 +1,11 @@
+package com.plandiy.controller;
+
+import com.plandiy.model.issue.task.Task;
+
+public class EditTaskController {
+    private Task task;
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}

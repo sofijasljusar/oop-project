a9bf568a3c712e1ead73d4722ad80bc7fae52ef0
sofijasljusar <sofija.sljusar@gmail.com>
2025-05-03 17:46:17 +0300

    Implement RiskManager class

--- src/main/java/com/plandiy/service/risk/RiskManager.java
+++ src/main/java/com/plandiy/service/risk/RiskManager.java
@@ -1,4 +1,42 @@
 package com.plandiy.service.risk;
 
+import com.plandiy.model.issue.task.Task;
+import com.plandiy.model.project.Project;
+import com.plandiy.service.risk.factory.DeadlineRiskCreator;
+import com.plandiy.service.risk.factory.RiskCreator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RiskManager {
+    private final List<RiskCreator> creators = List.of(
+            new DeadlineRiskCreator()
+    );
+    private final List<Risk> risks = new ArrayList<>();
+
+    public void identifyRisks(Project project) {
+        for (Task task: project.getListOfTasks()) {
+            for (RiskCreator creator: creators) {
+                Risk risk = creator.createRisk(task);
+                if (risk != null && risk.getImpact() > 0.5 && risk.getProbability() > 0.5) {
+                    risks.add(risk);
+                }
+            }
+        }
+    }
+
+    public void evaluateRisks() {
+        for (Risk risk: risks) {
+            System.out.printf("%s | Prob: %.2f | Impact: %.2f%n",
+                    risk.getDescription(), risk.getProbability(), risk.getImpact());
+        }
+    }
+
+    public void manageRisks() {
+        for (Risk risk : risks) {
+            System.out.println("Task: " + risk.getDescription() +
+                    " -> Strategy: " + risk.getMitigationStrategy());
+        }
+    }
+
 }
